5f696ffce060064e8be6e12de004ec64c6b26949
Tanujay Singh <tanujaysingh063@gmail.com>
2024-10-14 12:44:03 +0530

    new css for homepage

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,6 +3,7 @@ import Calendar from 'react-calendar';
 import { Box, Typography, Paper, styled } from '@mui/material';
 import Grid from '@mui/material/Grid2';
 import 'react-calendar/dist/Calendar.css';
+import './home.css';
 
 export default function Home() {
     const [value, onChange] = React.useState(new Date());
@@ -16,21 +17,21 @@ export default function Home() {
     }));
 
     return (
-        <Box sx={{ textAlign: 'center', marginTop: 5, flexGrow: 1 }}>
+        <Box sx={{ textAlign: 'center', marginTop: 5, flexGrow: 1 }} className='boxContainer'>
             <Grid 
                 container 
                 spacing={2} 
                 justifyContent="center" 
                 alignItems="center"
                 sx={{ height: '80vh' }}  
-            >
+            >   
                 
-                <Grid item xs={12} md={4}>
+                <Grid item xs={12} md={4} className='headingTexts'>
                     <Item>
-                        <Typography variant="h3" component="h1" gutterBottom>
+                        <Typography variant="h3" component="h1" gutterBottom className='boxTextTop'>
                             Welcome to the Meeting Scheduler
                         </Typography>
-                        <Typography variant="body1" component="p" gutterBottom>
+                        <Typography variant="body1" component="p" gutterBottom className='boxTextBottom'> 
                             Here you can manage your availability and view upcoming meetings. Please proceed to the User Page to manage your schedules.
                         </Typography>
                     </Item>
@@ -39,7 +40,7 @@ export default function Home() {
            
                 <Grid item xs={12} md={4}>
                     <Item>
-                        <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+                        <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center', backgroundColor: 'transparent' }}>
                             <Calendar
                                 onChange={onChange}
                                 value={value}
